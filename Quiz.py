7951b0f587b79d93480dd64ffe04e48bf5e1c7c3
HiranoK <45801682+HiranoK@users.noreply.github.com>
2019-02-12 10:02:02 -1000

    Create Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,12 @@
+origianl_questions = {
+  #Format is 'question':[options]
+  'According to the new 2017 NIST guidelines, how often should you change your password?':['Never','Every month','Every 6 months','Every year'],
+ 'Great Wall of China':['China','Beijing','Shanghai','Tianjin'],
+ 'Petra':['Ma\'an Governorate','Amman','Zarqa','Jerash'],
+ 'Machu Picchu':['Cuzco Region','Lima','Piura','Tacna'],
+ 'Egypt Pyramids':['Giza','Suez','Luxor','Tanta'],
+ 'Colosseum':['Rome','Milan','Bari','Bologna'],
+ 'Christ the Redeemer':['Rio de Janeiro','Natal','Olinda','Betim']
+}
+
+print("Hello")
