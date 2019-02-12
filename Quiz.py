63881b5e5f2e164aeb8e87104bb6289363391961
HiranoK <45801682+HiranoK@users.noreply.github.com>
2019-02-12 10:38:52 -1000

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,12 +1,28 @@
-origianl_questions = {
+import copy, random
+
+original_questions = {
   #Format is 'question':[options]
   'According to the new 2017 NIST guidelines, how often should you change your password?':['Never','Every month','Every 6 months','Every year'],
- 'Great Wall of China':['China','Beijing','Shanghai','Tianjin'],
- 'Petra':['Ma\'an Governorate','Amman','Zarqa','Jerash'],
- 'Machu Picchu':['Cuzco Region','Lima','Piura','Tacna'],
- 'Egypt Pyramids':['Giza','Suez','Luxor','Tanta'],
- 'Colosseum':['Rome','Milan','Bari','Bologna'],
- 'Christ the Redeemer':['Rio de Janeiro','Natal','Olinda','Betim']
 }
 
-print("Hello")
+questions = copy.deepcopy(original_questions)
+
+def shuffle(q):
+  selected_keys = []
+  i = 0
+  while i < len(q):
+    current_selection = random.choice(q.keys())
+    if current_selection not in selected_keys:
+      selected_keys.append(current_selection)
+      i = i+1
+  return selected_keys
+
+questions_shuffled = shuffle(questions)
+
+for i in questions_shuffled:
+ random.shuffle(questions[i])
+ print('''
+ Where is {} located?
+ {}
+ Correct Answer is: {}
+ ''').format(i,questions[i],original_questions[i][0])
